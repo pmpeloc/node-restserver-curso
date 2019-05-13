0c0123f5c6f020278bcf5391f2ff32c3774a0539
Pablo Misael Peloc <pmpeloc@gmail.com>
2019-05-13 20:04:08 -0300

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,23 @@
 // =============================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// =============================
+// ENTORNO  
+// =============================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =============================
+// BASE DE DATOS  
+// =============================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-node:123456-Cafe@ds155626.mlab.com:55626/cafe';
+}
+
+process.env.URLDB = urlDB;
